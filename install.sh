d2738aece08f89e769ae74bac89f8e7b8ff66ccd
yukina3230 <75545944+yukina3230@users.noreply.github.com>
2023-05-30 00:49:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 # Create Folders
-mkdir $HOME/Documents/Archive
-mkdir $HOME/Documents/Ebooks
-mkdir $HOME/Documents/Journals
-mkdir $HOME/Documents/Projects
-mkdir $HOME/Pictures/Icons
-mkdir $HOME/Pictures/Memes
-mkdir $HOME/Pictures/Artworks
-mkdir $HOME/Pictures/Wallpapers
-mkdir $HOME/Pictures/Screenshots
+mkdir $HOME/Documents/Archive/
+mkdir $HOME/Documents/Ebooks/
+mkdir $HOME/Documents/Journals/
+mkdir $HOME/Documents/Projects/
+mkdir $HOME/Pictures/Icons/
+mkdir $HOME/Pictures/Memes/
+mkdir $HOME/Pictures/Artworks/
+mkdir $HOME/Pictures/Wallpapers/
 
 # Set Aliases
 alias -s lock="xdg-screensaver lock"
@@ -19,21 +18,21 @@ alias -s update-init="sudo mkinitcpio -p linux"
 alias -s session-type="echo $XDG_SESSION_TYPE"
 
 # System Fonts
-sudo cp -a "$HOME/arch/arch-font/Bookerly/." /usr/share/fonts/TTF
-sudo cp -a "$HOME/arch/arch-font/Comic Sans/." /usr/share/fonts/TTF
-sudo cp -a "$HOME/arch/arch-font/Google Sans (2020)/." /usr/share/fonts/TTF
-sudo cp -a "$HOME/arch/arch-font/Google Sans Text (IO 2021)/." /usr/share/fonts/TTF
-sudo cp -a "$HOME/arch/arch-font/Gotham/." /usr/share/fonts/TTF
-sudo cp -a "$HOME/arch/arch-font/Gotham Rounded/." /usr/share/fonts/TTF
-sudo cp -a "$HOME/arch/arch-font/Montserrat/." /usr/share/fonts/TTF
-sudo cp -a "$HOME/arch/arch-font/P22 Typewriter/." /usr/share/fonts/TTF
+sudo cp -a "$HOME/arch/font/Bookerly/." /usr/share/fonts/TTF/
+sudo cp -a "$HOME/arch/font/Comic Sans/." /usr/share/fonts/TTF/
+sudo cp -a "$HOME/arch/font/Google Sans (2020)/." /usr/share/fonts/TTF/
+sudo cp -a "$HOME/arch/font/Google Sans Text (IO 2021)/." /usr/share/fonts/TTF/
+sudo cp -a "$HOME/arch/font/Gotham/." /usr/share/fonts/TTF/
+sudo cp -a "$HOME/arch/font/Gotham Rounded/." /usr/share/fonts/TTF/
+sudo cp -a "$HOME/arch/font/Montserrat/." /usr/share/fonts/TTF/
+sudo cp -a "$HOME/arch/font/P22 Typewriter/." /usr/share/fonts/TTF/
 
 # Activities Icon & GDM Logo
-sudo cp $HOME/arch/arch-icon/archlinux-icon.svg /usr/share/icons
-sudo cp $HOME/arch/arch-icon/archlinux.svg /usr/share/icons
-sudo cp $HOME/arch/arch-config/gdm/gdm /etc/dconf/profile
-sudo mkdir /etc/dconf/db/gdm.d
-sudo cp $HOME/arch/arch-config/gdm/01-logo /etc/dconf/db/gdm.d
+sudo cp $HOME/arch/icon/archlinux-icon.svg /usr/share/icons/
+sudo cp $HOME/arch/icon/archlinux.svg /usr/share/icons/
+sudo cp $HOME/arch/config/gdm/gdm /etc/dconf/profile/
+sudo mkdir /etc/dconf/db/gdm.d/
+sudo cp $HOME/arch/config/gdm/01-logo /etc/dconf/db/gdm.d/
 sudo dconf update
 
 # Disable GNOME Shell's version check
@@ -43,34 +42,33 @@ gsettings set org.gnome.shell disable-extension-version-validation true
 gsettings set org.gnome.desktop.sound event-sounds false
 
 # Grub Theme
-sudo cp -r $HOME/arch/arch-grub/archlinux /usr/share/grub/themes
-sudo cp $HOME/arch/arch-config/grub/grub /etc/default
+sudo cp -r $HOME/arch/grub/archlinux/ /usr/share/grub/themes/
+sudo cp $HOME/arch/config/grub/grub /etc/default/
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # Plymouth Theme
-#sudo cp -r $HOME/arch/arch-plymouth/archlinux /usr/share/plymouth/themes
-sudo cp $HOME/arch/arch-config/mkinitcpio/mkinitcpio.conf /etc/mkinitcpio.conf
+#sudo cp -r $HOME/arch/plymouth/archlinux /usr/share/plymouth/themes/
+sudo cp $HOME/arch/config/mkinitcpio/mkinitcpio.conf /etc/
 sudo plymouth-set-default-theme -R archlinux
 
-# Binary Files
-# anonfile v1.0.0
-cp $HOME/arch/arch-bin/anonfile $HOME/.local/bin
-
-# Neofetch Config
-cp $HOME/arch/arch-config/neofetch/config.conf $HOME/.config
-
 # zram Config
-sudo cp $HOME/arch/arch-config/zram/zram-generator.conf /etc/systemd
+sudo cp $HOME/arch/config/zram/zram-generator.conf /etc/systemd/
 
 # TLP Config
-sudo cp $HOME/arch/arch-config/tlp/tlp.conf /etc
+sudo cp $HOME/arch/config/tlp/tlp.conf /etc/
 
 # Reflector Config
-sudo cp $HOME/arch/arch-config/reflector/reflector.conf /etc/xdg/reflector
+sudo cp $HOME/arch/config/reflector/reflector.conf /etc/xdg/reflector/
+
+# Neofetch Config
+cp $HOME/arch/config/neofetch/config.conf $HOME/.config/
+
+# mpv Config
+cp -r $HOME/arch/config/mpv/ $HOME/.config/
 
 # Template Files
-cp -a $HOME/arch/arch-template/. $HOME/Templates
+cp -a $HOME/arch/template/. $HOME/Templates/
 
 # Sample Files
-cp $HOME/arch/arch-config/sample/hack $HOME/.hack
-sudo cp $HOME/arch/arch-config/sample/amogus.cow /usr/share/cows
+cp $HOME/arch/config/sample/hack $HOME/.hack
+sudo cp $HOME/arch/config/sample/amogus.cow /usr/share/cows/
